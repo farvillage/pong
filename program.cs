f71aeafbd0fe411f74b259871539e9d1a9231b48
Ygor Carvalho <ygorsoares2003@gmail.com>
2023-04-18 11:31:31 -0300

    first commit

---
+++ program.cs
@@ -0,0 +1,18 @@
+using (System);
+
+namespace csharp01
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Ygor\nCarvalho");
+
+            Console.ReadKey();
+        }
+
+    }
+}
